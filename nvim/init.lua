b99c583f13fd8c0a2b1e5d6d4acbaad50fe9017c
luked1999 <lucas.darling1@gmail.com>
2026-04-17 19:37:44 -0400

    updated nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,7 @@
 vim.g.mapleader = " "
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -26,13 +29,24 @@ require("lazy").setup({
   { "williamboman/mason.nvim", config = function() require("mason").setup() end },
   { "williamboman/mason-lspconfig.nvim" },
   { "neovim/nvim-lspconfig" },
+  { "nvim-tree/nvim-tree.lua",
+      version = "*",
+      lazy = false,
+      dependencies = { "nvim-tree/nvim-web-devicons" },
+      config = function()
+      require("nvim-tree").setup({})
+  end,
+},
+  
 })
 
 require('lucas.remap')
 require('lucas.lsp')
 
+vim.opt.termguicolors = true
 vim.o.cursorline = true
-vim.o.number = true
+vim.opt.number = true
+vim.opt.relativenumber = true
 vim.g.clipboard = {
   copy  = { ["+"] = "wl-copy", ["*"] = "wl-copy --primary" },
   paste = { ["+"] = "wl-paste", ["*"] = "wl-paste --primary" },
--- nvim/lua/lucas/remap.lua
+++ nvim/lua/lucas/remap.lua
@@ -1,2 +1,2 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>tt", "<Cmd>ToggleTerm<CR>", { desc = "Toggle terminal" })
+vim.keymap.set("n", "<leader>pv", "<Cmd>NvimTreeToggle<CR>", { desc = "Toggle nvim tree" })
